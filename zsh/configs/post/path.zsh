86bf74d4732c636e9cff14e81a68f96a0bffae08
David Alexander <davidpaulalexander@gmail.com>
2016-09-19 13:42:23 -0400

    Updating pre-/post-rcup hooks and package installers
    - ZSH paths
    - Adding Golang and $GOPATH for tag CLI tool
    - Updating apt and brew packages from current workstation
    - Adding user-specific npm install "global" directory to userspace
    - Use Chruby everywhere instead of rbenv
    - Ruby-install stores ruby source code in hidden directory
    - Install default ruby gems
    - Install default npm packages
    - Add instructions for importing PGP keys

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -6,11 +6,6 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
-# load rbenv if available
-if command -v rbenv >/dev/null; then
-  eval "$(rbenv init - --no-rehash)"
-fi
-
 # `mkdir -p .git/safe' in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
