37c65a4cd69e90d3b64e1d773bc094269f30359f
David Alexander <opensource@thelonelyghost.com>
2016-10-26 20:50:50 -0400

    Chruby autoswitch + ./.git/safe/../../bin hack

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -6,7 +6,4 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
-# `mkdir -p .git/safe' in the root of repositories you trust
-PATH=".git/safe/../../bin:$PATH"
-
 export -U PATH
