53b717d30c03ad74ca2c8547e3f37c55a69fe1c1
David Alexander <opensource@thelonelyghost.com>
2017-03-15 12:52:33 -0400

    Shellcheck fixes for zsh configs

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,7 +1,7 @@
 # PATH changes
-[ -f "$HOME/.path" ] && \
-  source "$HOME/.path" \
-  || true
+if [ -f "${HOME}/.path" ]; then
+  . "${HOME}/.path"
+fi
 
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:$HOME/.local/bin:/usr/local/sbin:$PATH"
