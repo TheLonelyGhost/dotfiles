79290cd89e508513f917ada6f888ce31da6998b0
David Alexander <opensource@thelonelyghost.com>
2017-04-05 19:12:37 -0400

    Changing CWD has `ls` with colors cross-platform

--- zsh/configs/post/chpwd.zsh
+++ zsh/configs/post/chpwd.zsh
@@ -1,9 +1,19 @@
 
+if ls --color=auto &>/dev/null; then
+  export HAS_MAC_LS=false
+else
+  export HAS_MAC_LS=true
+fi
+
 # Default actions upon changing directory
 function chpwd {
   # only present new directory's ls if in the terminal, not when scripted out
   if [ -t 1 ]; then
-    ls
+    if $HAS_MAC_LS; then
+      ls -GF
+    else
+      ls -F --color=auto
+    fi
   fi
 }
 
