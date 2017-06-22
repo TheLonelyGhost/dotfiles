64c06ca48bd3240c8bf3c411ce3ee298b0dc416c
David Alexander <opensource@thelonelyghost.com>
2017-06-22 10:44:19 -0400

    Reworks terminal colors to handle iTerm2 stuff
    Bug with ZSH colors module such that it doesn't know how to interpret
    the `$fg_color[green]` if the iTerm2 color scheme is changed to, e.g.,
    Solarized Dark. It works just fine in Dark Background (the default)
    though.

--- zsh/configs/color.zsh
+++ zsh/configs/color.zsh
@@ -1,6 +1,23 @@
 # makes color constants available
-autoload -U colors
-colors
+# autoload -U colors
+# colors
+
+RESET_COLOR=$(tput sgr0)
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
 
 # enable colored output from ls, etc. on FreeBSD-based systems
 export CLICOLOR=1
