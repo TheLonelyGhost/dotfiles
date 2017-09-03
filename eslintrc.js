bdafe202c774a480b5a77a2d59d365b7429aa757
David Alexander <opensource@thelonelyghost.com>
2017-09-03 12:59:03 -0400

    ESLint should warn, not error on wrong quotes

--- eslintrc.js
+++ eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   "rules": {
     "indent": [ "error", 2 ],
     "linebreak-style": [ "error", "unix" ],
-    "quotes": [ "error", "double" ],
+    "quotes": [ "warn", "double" ],
     "semi": [ "error", "always" ]
   }
 };
