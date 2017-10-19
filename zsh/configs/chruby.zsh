e888499b11a533c22c1c43e1b9a4e70418e2fd68
David Alexander <opensource@thelonelyghost.com>
2017-10-19 18:53:58 -0400

    Replaces chruby auto-switcher with direnv

--- zsh/configs/chruby.zsh
+++ zsh/configs/chruby.zsh
@@ -2,7 +2,3 @@
 [ -f "/usr/local/share/chruby/chruby.sh" ] && \
   source "/usr/local/share/chruby/chruby.sh" \
   || true
-# Load chruby auto-switching option with .ruby-version files
-[ -f "/usr/local/share/chruby/auto.sh" ] && \
-  source "/usr/local/share/chruby/auto.sh" \
-  || true
--- zsh/configs/post/git_safe.zsh
+++ zsh/configs/post/git_safe.zsh
@@ -1,28 +1,6 @@
-# SEE: http://dance.computer.dance/posts/2015/02/making-chruby-and-binstubs-play-nice.html
+
+# Replaced with adding an .envrc to the repository with the contents for trusted repositories:
 #
-# Chruby + ./.git/safe/../../bin hack
-
-# Remove the need for bundle exec ... or ./bin/...
-# by adding ./bin to path if the current project is trusted
-
-function set_local_bin_path() {
-  # Replace any existing local bin paths with our new one
-  export PATH="${1:-""}`echo "$PATH"|sed -e 's,[^:]*\.git/[^:]*bin:,,g'`"
-}
-
-function add_trusted_local_bin_to_path() {
-  if [ -d "$PWD/.git/safe" ]; then
-    # We're in a trusted project directory so update our local bin path
-    set_local_bin_path "$PWD/.git/safe/../../bin:$PWD/.git/safe/../../node_modules/.bin:"
-  fi
-}
-
-# Make sure add_trusted_local_bin_to_path runs after chruby so we
-# prepend the default chruby gem paths
-if [ -n "$ZSH_VERSION" ]; then
-  if [[ ! "$preexec_functions" == *add_trusted_local_bin_to_path* ]]; then
-    preexec_functions+=("add_trusted_local_bin_to_path")
-  fi
-fi
-
+#     PATH_add "${PWD}/bin"
+#
 
