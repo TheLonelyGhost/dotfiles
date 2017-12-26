177a80a4f3144f1840404cb421db3eff9ca0f0b9
David Alexander <opensource@thelonelyghost.com>
2017-12-26 16:00:14 -0500

    Adds Ruby OR Python environment info to $PS1

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,14 +1,23 @@
 # modify the prompt to contain git branch name if applicable
+join_by() { local IFS="$1"; shift; echo "$*"; }
 git_prompt_info() {
   current_branch=$(git current-branch 2> /dev/null)
   if [[ -n $current_branch ]]; then
     echo " %{$GREEN%}$current_branch%{$NORMAL%}"
   fi
 }
-show_virtual_env_name() {
+show_prefix_name() {
+  local out=()
   if [ -n "${VIRTUAL_ENV:-}" ]; then
-    echo "($(basename "${VIRTUAL_ENV}"))"
+    out+="$(basename "${VIRTUAL_ENV}")"
+  elif [ -n "${RUBY_VERSION:-}" ]; then
+    out+="${RUBY_ENGINE:-ruby}-${RUBY_VERSION}"
+  fi
+
+  if [ "${#out}" -gt 0 ]; then
+    echo "($(join_by , "${out[@]}"))"
   fi
 }
+
 setopt promptsubst
-PS1='$(show_virtual_env_name)${SSH_CONNECTION+"%{$GREEN%}%n@%m:"}%{$BLUE%}%c%{$NORMAL%}$(git_prompt_info) %# '
+PS1='$(show_prefix_name)${SSH_CONNECTION+"%{$GREEN%}%n@%m:"}%{$BLUE%}%c%{$NORMAL%}$(git_prompt_info) %# '
