1a4a62715e494d0a247916574b59924ddbc89eba
David Alexander <opensource@thelonelyghost.com>
2018-04-01 21:11:21 -0400

    Adding all ssh keys happens through custom, external command

--- zsh/configs/keychain.zsh
+++ zsh/configs/keychain.zsh
@@ -1,5 +1,5 @@
 # vim: ft=zsh
-find "${HOME}/.ssh" -type f -iname '*id_rsa' -print0 | xargs -0 -I{} keychain --nogui --quiet --agents 'ssh,gpg' {}
+keychain --nogui --quiet --agents 'ssh,gpg'
 
 # See ~/.zsh/conf/post/chpwd.zsh to see we'll source this file on every change in directory because
 # it's fast and an easy way to keep all the terminal tabs in sync with the latest GPG/SSH agent PIDs
@@ -9,3 +9,5 @@ fi
 if [ -e "${HOME}/.keychain/$(uname -n)-sh-gpg" ]; then
   . "${HOME}/.keychain/$(uname -n)-sh-gpg"
 fi
+
+if command -v add-all-ssh-keys &>/dev/null; then add-all-ssh-keys; fi
