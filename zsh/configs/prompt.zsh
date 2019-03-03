55399d7aab5e1027f47b6b394b9a4c316f281bd5
David Alexander <opensource@thelonelyghost.com>
2019-03-03 12:08:38 -0500

    Reworks prompt to take full advantage of ZSH stuff

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,13 +1,41 @@
 # modify the prompt to contain git branch name if applicable
-join_by() { local IFS="$1"; shift; echo "$*"; }
-git_prompt_info() {
-  current_branch=$(git current-branch 2> /dev/null)
-  if [ -n "$current_branch" ]; then
-    echo " %{$GREEN%}$current_branch%{$NORMAL%}"
-  fi
+# git_prompt_info() {
+#   current_branch=$(git current-branch 2> /dev/null)
+#   if [ -n "$current_branch" ]; then
+#     echo " %{$GREEN%}$current_branch%{$NORMAL%}"
+#   fi
+# }
+
+fpath=("${HOME}/.zsh/themes" $fpath)
+setopt promptsubst
+autoload -U vcs_info
+autoload -Uz promptinit
+
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:git:default:*' check-for-changes true
+
+zstyle ':vcs_info:git:default:*' formats '%s::(%c%{'"$GREEN"'%}%b%{'"$NORMAL"'%}%u)'
+zstyle ':vcs_info:git:default:*' stagedstr '%{'"$BRIGHT$YELLOW"'%}^%{'"$NORMAL"'%}'
+zstyle ':vcs_info:git:default:*' unstagedstr '%{'"$BRIGHT$BLUE"'%}*%{'"$NORMAL"'%}'
+
+# Coffee cup emoji: 'staged'
+# zstyle ':vcs_info:git:default:*' stagedstr "$(printf ' %b\n' "\U2615")"
+# "No" sign emoji: 'unstaged'
+# zstyle ':vcs_info:git:default:*' unstagedstr "$(printf ' %b\n' "\U1F6AB")"
+
+promptinit
+precmd() { # Hook for us to get the version control info in the prompt
+  vcs_info
 }
-show_prefix_name() {
-  local out=()
+
+# set our prompt info
+prompt thelonelyghost
+
+# shell functions that should exist for the PROMPT
+__show-prefix() {
+  local IFS out
+  out=()
+
   if [ -n "${VIRTUAL_ENV:-}" ] && [ "${PIPENV_ACTIVE:-}" -ne '1' ]; then
     out+="$(basename "${VIRTUAL_ENV}")"
   fi
@@ -16,9 +44,7 @@ show_prefix_name() {
   fi
 
   if [ "${#out}" -gt 0 ]; then
-    echo "($(join_by , "${out[@]}")) "
+    IFS='|'
+    printf '(%s) \n' "${out[*]}"
   fi
 }
-
-setopt promptsubst
-PS1='$(show_prefix_name)${SSH_CONNECTION+"%{$GREEN%}%n@%m:"}%{$BLUE%}%c%{$NORMAL%}$(git_prompt_info) %# '
