e61cb44bd4d519799f67212e0b00091085873478
David Alexander <opensource@thelonelyghost.com>
2019-03-04 11:32:54 -0500

    vcs_info is the devil. Just use a custom solution for git

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -8,25 +8,9 @@
 
 fpath=("${HOME}/.zsh/themes" $fpath)
 setopt promptsubst
-autoload -U vcs_info
 autoload -Uz promptinit
 
-zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:git:default:*' check-for-changes true
-
-zstyle ':vcs_info:git:default:*' formats '%s::(%c%{'"$GREEN"'%}%b%{'"$NORMAL"'%}%u)'
-zstyle ':vcs_info:git:default:*' stagedstr '%{'"$BRIGHT$YELLOW"'%}^%{'"$NORMAL"'%}'
-zstyle ':vcs_info:git:default:*' unstagedstr '%{'"$BRIGHT$BLUE"'%}*%{'"$NORMAL"'%}'
-
-# Coffee cup emoji: 'staged'
-# zstyle ':vcs_info:git:default:*' stagedstr "$(printf ' %b\n' "\U2615")"
-# "No" sign emoji: 'unstaged'
-# zstyle ':vcs_info:git:default:*' unstagedstr "$(printf ' %b\n' "\U1F6AB")"
-
 promptinit
-precmd() { # Hook for us to get the version control info in the prompt
-  vcs_info
-}
 
 # set our prompt info
 prompt thelonelyghost
@@ -46,5 +30,35 @@ __show-prefix() {
   if [ "${#out}" -gt 0 ]; then
     IFS='|'
     printf '(%s) \n' "${out[*]}"
+  fi
+}
+
+# This is a lighter-weight version of what vcs_info already provides
+git_prompt_info() {
+  local current_branch dirty_indicator staged_indicator ret empty_tree_sha HEAD
+  dirty_indicator=''
+  staged_indicator=''
+  empty_tree_sha='4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+  current_branch=$(git current-branch 2> /dev/null)
+  if [ -n "$current_branch" ]; then
+    if ! git diff --no-ext-diff --ignore-submodules=dirty --quiet --exit-code 2> /dev/null; then
+			# YEP! We have some tracked, but unstaged changes!
+      dirty_indicator=' %{'"$BRIGHT$RED"'%}✗%{'"$NORMAL"'%}'
+    fi
+    if git rev-parse --quiet --verify HEAD &> /dev/null; then
+      # Repo exists
+			HEAD='HEAD'
+    else
+      # Empty repo (no commits
+			HEAD="${empty_tree_sha}"
+    fi
+
+		ret=$(git diff-index --cached --quiet --ignore-submodules=dirty HEAD 2> /dev/null || echo $?)
+		if [ "$ret" -ne 128 ] && [ "$ret" -ne 0 ]; then
+			# YEP! We have some staged changes!
+			staged_indicator=' %{'"$BRIGHT$GREEN"'%}✔%{'"$NORMAL"'%}'
+		fi
+
+    printf 'git::(%b%s%b)%b%b\n' "%{${BLUE}%}" "$current_branch" "%{$NORMAL%}" "${staged_indicator}" "${dirty_indicator}"
   fi
 }
