84d2df480e3509bf75040a04cdaa516345004f7a
David Alexander <opensource@thelonelyghost.com>
2019-10-25 22:02:14 -0400

    Pipsi is dead, long live pipx!

--- zsh/configs/post/pyenv.zsh
+++ zsh/configs/post/pyenv.zsh
@@ -2,6 +2,10 @@ if type pyenv &>/dev/null; then
   eval "$(pyenv init -)"
 fi
 
+if [ -e "${HOME}/.local/bin" ]; then
+  PATH="${HOME}/.local/bin:${PATH}"
+fi
+
 if [ -e "${HOME}/.poetry/bin" ]; then
   PATH="${HOME}/.poetry/bin:$PATH"
 fi
