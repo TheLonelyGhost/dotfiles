b90df301e99c8087e9270832d5f7c6b6cb9bf806
David Alexander <opensource@thelonelyghost.com>
2020-02-13 11:58:12 -0500

    Gets pipx and poetry configs in order

--- zsh/configs/post/pyenv.zsh
+++ zsh/configs/post/pyenv.zsh
@@ -6,10 +6,6 @@ if [ -e "${HOME}/.local/bin" ]; then
   PATH="${HOME}/.local/bin:${PATH}"
 fi
 
-if [ -e "${HOME}/.poetry/bin" ]; then
-  PATH="${HOME}/.poetry/bin:$PATH"
-fi
-
 if [ ! -e "${HOME}/.zsh/configs/post/pipx.zsh" ]; then
   register-python-argcomplete pipx > "${HOME}/.zsh/configs/post/pipx.zsh"
 fi
