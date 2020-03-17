f28cabb1a1289dce588dd34b299864d573bf0b88
David Alexander <opensource@thelonelyghost.com>
2020-03-16 23:28:44 -0400

    Continues moving stuff out of `direnvrc` directly

--- config/direnv/lib.sh
+++ config/direnv/lib.sh
@@ -25,3 +25,12 @@ if ! has log_error; then
   }
 fi
 
+# Project-specific vim configurations
+use_customized_vim() {
+  local extravim
+  extravim="$(find_up .vimrc)"
+  if [ -n "$extravim" ] && [ "$extravim" != "${HOME}/.vimrc" ]; then
+    log_status "Adding vim configurations: ${extravim}"
+    path_add EXTRA_VIM "$extravim"
+  fi
+}
