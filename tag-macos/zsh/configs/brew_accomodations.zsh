78744343d0565a96530a0f298120c99ee55bfed7
David Alexander <opensource@thelonelyghost.com>
2020-04-08 16:46:40 -0400

    Use GNU util variants by default instead of BSD variants

--- tag-macos/zsh/configs/brew_accomodations.zsh
+++ tag-macos/zsh/configs/brew_accomodations.zsh
@@ -1,3 +1,13 @@
 fpath=(/usr/local/share/zsh/site-functions $fpath)
 
+# Use GNU coreutils unprefixed
+PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+MANPATH="/usr/local/opt/coreutils/gnuman:${MANPATH}"
+
+# Use GNU make unprefixed
+PATH="/usr/local/opt/make/libexec/gnubin:${PATH}"
+MANPATH="/usr/local/opt/make/gnuman:${MANPATH}"
+
+export SSL_CERT_FILE='/usr/local/etc/openssl/cert.pem'
+
 alias brew='env PYENV_VERSION=system brew'
