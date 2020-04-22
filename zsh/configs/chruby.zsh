49746af31f174c17d4997a235868e5f235fd9085
David Alexander <opensource@thelonelyghost.com>
2020-04-22 02:48:21 -0400

    Extracts more configs to zsh-base

--- zsh/configs/chruby.zsh
+++
@@ -1,6 +0,0 @@
-# Load chruby if available
-if [ -f "${HOME}/.nix-profile/share/chruby/chruby.sh" ]; then
-  source "${HOME}/.nix-profile/share/chruby/chruby.sh"
-elif [ -f "/usr/local/share/chruby/chruby.sh" ]; then
-  source "/usr/local/share/chruby/chruby.sh"
-fi
--- zsh/configs/zle_config.zsh
+++
@@ -1,9 +0,0 @@
-# vim: ft=zsh
-
-bindkey -A viins main
-
-bindkey '^F' forward-word
-bindkey '^B' backward-word
-
-# Makes it so lines starting with '#' are ignored as if they are actual comment lines
-setopt interactive_comments mark_dirs
