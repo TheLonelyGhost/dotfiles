63d701d6e2c406e0be3088e061fee254d5fce811
David Alexander <opensource@thelonelyghost.com>
2020-04-22 11:25:11 -0400

    Moves zsh completion code inside of zplug framework

--- zsh/configs/post/completion.zsh
+++ zsh/configs/post/completion.zsh
@@ -1,14 +1,5 @@
-# load our own completion functions
-if [ -e /usr/local/share/zsh/site-functions ]; then
-  fpath=(/usr/local/share/zsh/site-functions $fpath)
-fi
-fpath=(~/.zsh/completion $fpath)
-
 # completion
-autoload -U compinit bashcompinit
-compinit
-bashcompinit
 
-# if [ $commands[kubectl] ]; then
-#   source <(kubectl completion zsh)
-# fi
+#autoload -U compinit bashcompinit
+#compinit
+#bashcompinit
