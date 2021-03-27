39f846b80f2a428a4a2bb6bb8bf265a2a9611216
David Alexander <opensource@thelonelyghost.com>
2021-03-26 21:33:42 -0400

    Fixes bug with asdf-java implementation clashing with direnv
    See also https://github.com/direnv/direnv/issues/726

--- zsh/configs/java.zsh
+++ zsh/configs/java.zsh
@@ -1,3 +1,9 @@
 if [ -e "${HOME}/.tool-versions" ] && grep -e '^java ' "${HOME}/.tool-versions" 1>/dev/null 2>&1; then
-  . "${HOME}/.asdf/plugins/java/set-java-home.zsh"
+  asdf_update_java_home() {
+    # shellcheck disable=SC2046
+    JAVA_HOME=$(realpath $(dirname $(readlink -f $(asdf which java)))/../)
+    export JAVA_HOME
+  }
+  autoload -U add-zsh-hook
+  add-zsh-hook precmd asdf_update_java_home
 fi
